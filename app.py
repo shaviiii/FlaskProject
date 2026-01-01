5d0a29a6030548e08879bfb5f21b8064ee99f00e
shaviiii <himanshiseth51@gmail.com>
2026-01-01 10:12:46 +0530

    abc

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello! This is a simple Flask app runing inside Docker.Himanshilsd2sd"
+    return "Hello! This is a simple Flask app runing inside Docker.Himanshilsd2sdabc"
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port=5000)
